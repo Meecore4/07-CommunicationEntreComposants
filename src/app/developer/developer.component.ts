2f7f6824db959d37638757c36b0fc4f00aad9afa
Meecore4 <[alicia.critelli4@gmail.com](mailto:alicia.critelli4@gmail.com)>
2023-01-24 15:22:52 +0100

    maj

--- src/app/developer/developer.component.ts
+++ src/app/developer/developer.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Developer } from '../models/developer';
-// import { Skill } from '../models/skill';
+import { Skill } from '../models/skill';
 
 @Component({
   selector: 'app-developer',
@@ -12,11 +12,7 @@ export class DeveloperComponent {
 myPerson = new Developer('Sandra', 'Conil', 36, 'female', 'sandra est gentille mais chiante', 
 [{name : 'Angular', logo : 'logomoche', site: 'teubé.com'},{name : 'Javascript', logo : 'logomoche', site : 'tarace.fr'}]);
 
-
-monTableauSkill = [{name : 'Angular', logo : 'logomoche', site: 'teubé.com'},{name : 'Javascript', logo : 'logomoche', site : 'tarace.fr'}];
   constructor(){}
 
-razzakMeTue = [{name: 'Razzak', logo:'logo'}, {name:'Sandra', logo:'logo'}]
+
 }
-
-
